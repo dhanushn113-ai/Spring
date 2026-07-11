9c34666c7eea17033dc29166268ae8705c681aef
dhanushn-113-ai <dhanushn113@gmail.com>
2026-07-11 17:43:27 +0530

    Add Spring Object Creation Project

---
+++ Spring Object Creation/src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Student {
+    public Student() {
+        System.out.println("Student is created");
+
+    }
+    public void show(){
+        System.out.println("In Show Method");
+    }
+}
